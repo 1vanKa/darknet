0823c748f12e72e8bf7b7f7c028ba9797181aff9
Nikita Shlyago <29282070+nikkita1267@users.noreply.github.com>
2022-01-25 17:27:51 +0300

    Improve log parser

--- scripts/log_parser/log_parser.py
+++ scripts/log_parser/log_parser.py
@@ -5,11 +5,14 @@
 # @File    : log_parser.py
 
 import argparse
+from datetime import datetime
 import logging
 import os
 import platform
 import re
 import sys
+from dataclasses import dataclass
+import typing as tp
 
 # set non-interactive backend default when os is not windows
 if sys.platform != 'win32':
@@ -18,6 +21,28 @@ if sys.platform != 'win32':
 
 import matplotlib.pyplot as plt
 from matplotlib.ticker import MultipleLocator, FormatStrFormatter
+
+
+@dataclass
+class RegionEntry:
+    normalizer_iou: float
+    normalizer_obj: float
+    normalizer_cls: float
+    iou: float
+    count: int
+    class_loss: float
+    iou_loss: float
+    total_loss: float
+
+
+@dataclass
+class LogEntry:
+    time: datetime
+    iteration: int
+    loss: float
+    average_loss: float
+    rate: float
+    region_values: tp.List[tp.Dict[int, RegionEntry]]
 
 
 def get_file_name_and_ext(filename):
@@ -40,11 +65,50 @@ def parse_args():
                         help='the directory to be saved')
     parser.add_argument('--csv-file', dest='csv_file', type=str, default="",
                         help='training log file')
-    parser.add_argument('--log-file', dest='log_file', type=str, default="",
+    parser.add_argument('--log-file', dest='log_file', type=str, default="test_yolo.log",
                         help='training log file')
-    parser.add_argument('--show', dest='show_plot', type=bool, default=False,
+    parser.add_argument('--show', dest='show_plot', type=bool, default=True,
                         help='whether to show')
     return parser.parse_args()
+
+
+def parse_between(
+        text: str,
+        cur_match: re.Match,
+        prev_match: tp.Optional[re.Match] = None
+) -> tp.List[tp.Dict[int, RegionEntry]]:
+    region_pattern = re.compile(r".*: \(iou: ([\d]*\.[\d]+), obj: ([\d]*\.[\d]+), cls: ([\d]*\.[\d]+)\) "
+                                r"Region ([\d]*) Avg \(IOU: ([\d]*\.[\d]+)\), count: ([\d]*), "
+                                r"class_loss = ([\d]*\.[\d]+), iou_loss = ([\d]*\.[\d]+), total_loss = ([\d]*\.[\d]+)")
+    if prev_match is None:
+        matches = region_pattern.findall(text[:cur_match.start()])
+    else:
+        matches = region_pattern.findall(text[prev_match.end(): cur_match.start()])
+
+    regions = []
+    cur_regions: tp.Dict[int, RegionEntry] = {}
+    last_region = -1
+    for match in matches:
+        cur_region = int(match[3])
+        entry = RegionEntry(float(match[0]), float(match[1]), float(match[2]),
+                            float(match[4]), int(match[5]), float(match[6]),
+                            float(match[7]), float(match[8]))
+        if cur_region < last_region:
+            regions.append(cur_regions)
+            cur_regions = {}
+        cur_regions[cur_region] = entry
+        last_region = cur_region
+    return regions
+
+
+def write_entry(out_file: tp.Any, entry: LogEntry) -> None:
+    date_str = datetime.strftime(entry.time, "%d.%m.%YT%H:%M:%S")
+    for regions in entry.region_values:
+        for key, region in regions.items():
+            out_file.write(f"{date_str},{entry.iteration},{entry.loss},{entry.average_loss},{entry.rate},"
+                           f"{key},{region.normalizer_iou},{region.normalizer_obj},{region.normalizer_cls},"
+                           f"{region.iou},{region.count},{region.iou_loss},{region.class_loss},"
+                           f"{region.total_loss}\n")
 
 
 def log_parser(args):
@@ -70,12 +134,9 @@ def log_parser(args):
     ax.yaxis.set_minor_locator(minor_locator)
     ax.yaxis.grid(True, which='minor')
 
-    pattern = re.compile(r"([\d].*): .*?, (.*?) avg")
-    # print(pattern.findall(log_content))
-    matches = pattern.findall(log_content)
-    # print(type(matches[0]))
+    pattern = re.compile(r"([\d].*): (.*?), (.*?) avg loss, (.*?) rate, .*? seconds, .*? images, .*? hours left")
+    matches = pattern.finditer(log_content)
     counter = 0
-    log_count = len(matches)
 
     if args.csv_file != '':
         csv_path = os.path.join(args.save_dir, args.csv_file)
@@ -84,17 +145,26 @@ def log_parser(args):
         csv_path = os.path.join(args.save_dir, file_name + '.csv')
         out_file = open(csv_path, 'w')
 
+    out_file.write("Time,Iteration,Loss,Avg Loss,Rate,Region,Norm IOU,Norm obj,Norm cls,IOU,Count,IOU Loss,Class Loss,"
+                   "Total Loss\n")
+
+    prev_match = None
     for match in matches:
         counter += 1
-        if log_count > 200:
-            if counter % 200 == 0:
-                print('parsing {}/{}'.format(counter, log_count))
+
+        if counter % 200 == 0:
+            print('parsing {}'.format(counter))
         else:
-            print('parsing {}/{}'.format(counter, log_count))
-        iteration, loss = match
+            print('parsing {}'.format(counter))
+        iteration, loss, avg_loss, rate = match.group(1), match.group(2), match.group(3), match.group(4)
+
+        regions = parse_between(log_content, match, prev_match)
+
         iterations.append(int(iteration))
         losses.append(float(loss))
-        out_file.write(iteration + ',' + loss + '\n')
+        write_entry(out_file, LogEntry(datetime.now(), int(iteration), float(loss), float(avg_loss), float(rate),
+                                       regions))
+        prev_match = match
 
     ax.plot(iterations, losses)
     plt.xlabel('Iteration')
